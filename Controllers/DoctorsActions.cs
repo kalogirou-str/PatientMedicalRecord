152cf4064abb1628b253c3dc16802b18f5133e46
kalogirou-str <61208259+kalogirou-str@users.noreply.github.com>
2023-10-15 23:23:14 +0300

    Just creation of doctor controller. No code included

---
+++ Controllers/DoctorsActions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientMedicalRecord.Controllers
+{
+    public class DoctorsActions : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
